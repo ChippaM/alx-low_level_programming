1256c48f54a7354c41001ecfd8bd23379388d1a1
ChippaM <chippa.mathebula@gmail.com>
2023-03-28 14:11:42 -0700

    correction

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-*function takes a pointer to an int as parameter and updates the value it points to to 98
+*reset_to_98 - takes a pointer to an int and updates the value it points to
 *@n: an integer
 */
 
